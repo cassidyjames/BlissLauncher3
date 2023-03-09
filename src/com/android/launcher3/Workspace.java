efe582fd2f5a97c10114ca876948024b575570a9
Yash Garg <yash.garg.ext@murena.com>
2023-03-09 14:23:26 +0530

    feat(navbar): add opacity animation for widgets page

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -35,6 +35,8 @@ import static com.android.launcher3.logging.StatsLogManager.LAUNCHER_STATE_HOME;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SWIPELEFT;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SWIPERIGHT;
 
+import static foundation.e.bliss.utils.BlissUtilsKt.createNavbarColorAnimator;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.LayoutTransition;
@@ -293,6 +295,8 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
 
     private final StatsLogManager mStatsLogManager;
 
+    private final ValueAnimator navbarAnimator;
+
     /**
      * Used to inflate the Workspace from XML.
      *
@@ -317,6 +321,9 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         mStateTransitionAnimation = new WorkspaceStateTransitionAnimation(mLauncher, this);
         mWallpaperManager = WallpaperManager.getInstance(context);
         mAllAppsIconSize = mLauncher.getDeviceProfile().allAppsIconSizePx;
+
+        navbarAnimator = createNavbarColorAnimator(mLauncher.getWindow());
+
         mWallpaperOffset = new WallpaperOffsetInterpolator(this);
 
         setHapticFeedbackEnabled(false);
@@ -1371,6 +1378,12 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
                                     LauncherAtom.WorkspaceContainer.newBuilder()
                                             .setPageIndex(prevPage)).build())
                     .log(event);
+
+            if (mCurrentPage == 0 && prevPage == 1) {
+                navbarAnimator.start();
+            } else if (prevPage == 0 && mCurrentPage == 1) {
+                navbarAnimator.reverse();
+            }
         }
     }
 
