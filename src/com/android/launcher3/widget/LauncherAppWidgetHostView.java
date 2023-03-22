108a273e86aeaf050e02afe989e0fb23307dec78
Yash Garg <yash.garg.ext@murena.com>
2023-03-22 20:48:38 +0530

    chore: setup for release builds
    * also fix some crashes

--- src/com/android/launcher3/widget/LauncherAppWidgetHostView.java
+++ src/com/android/launcher3/widget/LauncherAppWidgetHostView.java
@@ -286,6 +286,10 @@ public class LauncherAppWidgetHostView extends BaseLauncherAppWidgetHostView
         return info;
     }
 
+    public AppWidgetProviderInfo getProviderInfo() {
+        return super.getAppWidgetInfo();
+    }
+
     @Override
     public void onTouchComplete() {
         if (!mLongPressHelper.hasPerformedLongPress()) {
