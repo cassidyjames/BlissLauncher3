4494078aed7f1ef590d786812ecf0d96d7b94660
TheScarastic <warabhishek@e.email>
2023-04-11 19:41:30 +0530

    fix(folder): improve folder animations and give default name
    [SahilSonar - adapted to A14]
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/com/android/launcher3/folder/Folder.java
+++ src/com/android/launcher3/folder/Folder.java
@@ -243,7 +243,7 @@ public class Folder extends AbstractFloatingView implements ClipPathView, DragSo
     @Nullable
     private KeyboardInsetAnimationCallback mKeyboardInsetAnimationCallback;
 
-    private GradientDrawable mBackground;
+    public GradientDrawable mBackground;
 
     /**
      * Used to inflate the Workspace from XML.
@@ -1736,5 +1736,10 @@ public class Folder extends AbstractFloatingView implements ClipPathView, DragSo
 
     public int getUnusedOffsetYOnAnimate(boolean isOpening) {
         return 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View & ClipPathView> T getAnimateObject() {
+        return (T) this;
     }
 }
