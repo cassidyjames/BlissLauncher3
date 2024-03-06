b16cd7796bdf1c8dbf4cad0affdd54a334a3f07a
althafvly <althafvly@gmail.com>
2024-03-06 20:12:50 +0530

    feat: Remove rotation for phones

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -290,6 +290,8 @@ public class SettingsActivity extends FragmentActivity
                     if (info.isTablet(info.realBounds)) {
                         // Launcher supports rotation by default. No need to show this setting.
                         return false;
+                    } else {
+                        preference.setVisible(false);
                     }
                     // Initialize the UI once
                     preference.setDefaultValue(RotationHelper.getAllowRotationDefaultValue(info));
