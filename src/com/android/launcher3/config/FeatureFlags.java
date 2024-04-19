60b16351d6f40ab210b497c0df2afad2eeada91e
althafvly <althafvly@gmail.com>
2024-04-19 15:40:30 +0530

    Revert "feat: Use seperate activity for recents"
    This reverts commit 9ab81205af55f4f6844ff2ae8ee9169f7ff46656.

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -183,7 +183,7 @@ public final class FeatureFlags {
                     "Always use hardware optimization for folder animations.");
 
     public static final BooleanFlag SEPARATE_RECENTS_ACTIVITY = getDebugFlag(270392980,
-            "SEPARATE_RECENTS_ACTIVITY", true,
+            "SEPARATE_RECENTS_ACTIVITY", false,
             "Uses a separate recents activity instead of using the integrated recents+Launcher UI");
 
     public static final BooleanFlag ENABLE_MINIMAL_DEVICE = getDebugFlag(270392984,
