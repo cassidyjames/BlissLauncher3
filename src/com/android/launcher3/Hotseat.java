60869766bbc4ac07e5fdeb101d24decf4e59b5f7
althafvly <althafvly@gmail.com>
2024-04-19 10:55:56 +0530

    feat: Remove adding extra space on hotseat
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/com/android/launcher3/Hotseat.java
+++ src/com/android/launcher3/Hotseat.java
@@ -34,8 +34,6 @@ import android.view.ViewDebug;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import com.android.launcher3.folder.Folder;
-import com.android.launcher3.util.NavigationMode;
-import com.android.launcher3.util.window.WindowManagerProxy;
 
 import com.android.launcher3.util.HorizontalInsettableView;
 import com.android.launcher3.util.MultiTranslateDelegate;
@@ -200,8 +198,6 @@ public class Hotseat extends CellLayout implements Insettable, OffsetParent {
     public void setInsets(Rect insets) {
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) getLayoutParams();
         DeviceProfile grid = mActivity.getDeviceProfile();
-        WindowManagerProxy wm = WindowManagerProxy.newInstance(getContext());
-        boolean isFullyGesture = wm.getNavigationMode(getContext()) == NavigationMode.NO_BUTTON;
 
         if (grid.isVerticalBarLayout()) {
             mQsb.setVisibility(View.GONE);
@@ -217,11 +213,7 @@ public class Hotseat extends CellLayout implements Insettable, OffsetParent {
             mQsb.setVisibility(View.VISIBLE);
             lp.gravity = Gravity.BOTTOM;
             lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
-            if (isFullyGesture) {
-                lp.height = grid.hotseatBarSizePx;
-            } else {
-                lp.height = grid.hotseatBarSizePx + 24;
-            }
+            lp.height = grid.hotseatBarSizePx;
         }
 
         Rect padding = grid.getHotseatLayoutPadding(getContext());
