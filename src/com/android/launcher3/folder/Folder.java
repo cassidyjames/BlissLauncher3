1a3b0900338a037652201f27d9870d435b59183b
althafvly <althafvly@gmail.com>
2024-04-19 15:36:51 +0530

    feat: Hide folder from recent panel

--- src/com/android/launcher3/folder/Folder.java
+++ src/com/android/launcher3/folder/Folder.java
@@ -313,6 +313,13 @@ public class Folder extends AbstractFloatingView implements ClipPathView, DragSo
         return startDrag(v, new DragOptions());
     }
 
+    public boolean isFolderWobbling() {
+        Launcher launcher = mLauncherDelegate.getLauncher();
+        if (launcher != null) {
+            return launcher.getWorkspace().isWobbling();
+        } else return false;
+    }
+
     public boolean startDrag(View v, DragOptions options) {
         Object tag = v.getTag();
         if (tag instanceof WorkspaceItemInfo) {
